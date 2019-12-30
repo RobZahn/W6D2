b80d1a2b69b44820142713afd41c7d9232326ce9
Rob Zahn <rob.jzahn@gmail.com>
2019-12-30 17:43:37 -0500

    Add Phase1a

--- ActiveRecordLite/skeleton/lib/00_attr_accessor_object.rb
+++ ActiveRecordLite/skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,18 @@
+require 'byebug'
 class AttrAccessorObject
+  
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name| # => name is color currently
+      define_method(name) do
+        # debugger
+        instance_variable_get("@#{name}") #literally "@color"
+
+      end
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+
+    end
+
   end
 end
